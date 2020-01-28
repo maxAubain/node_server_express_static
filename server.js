5b7e049cbeac975dfae8a93c2406e40e2e497aac
Max Aubain <maxaubain@protonmail.com>
2020-01-28 13:12:35 -0500

    Server.js: refactors methods and adds comments

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+/* Express library */
 const express = require('express')
 const app = express()
-const expressHandlebars = require('express-handlebars')
+const port = process.env.PORT || 3000
 
-// configure Handlebars view engine
+/* Handlebars (view engine) */
+const expressHandlebars = require('express-handlebars')
 app.engine('handlebars', expressHandlebars({
   defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars')
 
-// const process.env.ALTPORT = 3030
-const port = process.env.ALTPORT || 3000
-
+/* API routes */
+// home
 app.get('/', (req, res) => {
   res.render('home')
 })
 
+// about
 app.get('/about', (req, res) => {
   res.render('about')
 })
@@ -22,22 +24,16 @@ app.get('/about', (req, res) => {
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404')
-
-  //or
-  //res.type('text/plain').status(404).send('404 - Not Found')
-
-  // or 
-  //res.sendStatus(404)
 })
 
 // 500 page
 app.use((req, res) => {
   console.error(err.message)
-  res.status(500)
-  res.render('500')
+  res.status(500).render('500')
 })
 
+/* Console */
+// Development console message while server is running
 app.listen(port, () => {
   console.log(`App is listening on https://localhost:${port}; ` + `press Ctrl-C to terminate.`)
-  // console.log(`'Process' object, 'Environment' property port: ${process.env.ALTPORT}`)
 })
